00ea705687bc03509f1b2cb40fa78f8493ada62d
mikulabarnabas <mikula.barnabas1234@gmail.com>
2026-04-26 21:11:08 +0200

    Better storage with cloudinary

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,6 +11,7 @@ use App\Observers\FoodsObserver;
 use App\Observers\WorkoutDiaryPivotObserver;
 use App\Models\UserProfile;
 use App\Observers\UserProfileObserver;
+use Cloudinary\Configuration\Configuration;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -31,5 +32,16 @@ class AppServiceProvider extends ServiceProvider
         WorkoutDiaryPivot::observe(WorkoutDiaryPivotObserver::class);
         Foods::observe(FoodsObserver::class);
         UserProfile::observe(UserProfileObserver::class);
+
+        Configuration::instance([
+            'cloud' => [
+                'cloud_name' => config('cloudinary.cloud_name'),
+                'api_key'    => config('cloudinary.api_key'),
+                'api_secret' => config('cloudinary.api_secret'),
+            ],
+            'url' => [
+                'secure' => true
+            ]
+        ]);
     }
 }
